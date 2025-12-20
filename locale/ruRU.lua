72168fb959707db6fd3b6ce84caf6db184b2b51d
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-12-20 09:15:26 +0300

    Create ruRU.lua

---
+++ locale/ruRU.lua
@@ -0,0 +1,16 @@
+local ADDON_NAME, addon = ...
+
+local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "ruRU")
+if not L then return end
+-- Translator ZamestoTV
+L.ChkBtnHealthOn = "xanSoundAlerts: Звуковые уведомления о здоровье теперь [|cFF99CC33ВКЛ|r]"
+L.ChkBtnHealthOff = "xanSoundAlerts: Звуковые уведомления о здоровье теперь [|cFF99CC33ВЫКЛ|r]"
+L.ChkBtnHealthInfo = "Включить звуковые уведомления о здоровье."
+
+L.ChkBtnManaOn = "xanSoundAlerts: Звуковые уведомления о мане теперь [|cFF99CC33ВКЛ|r]"
+L.ChkBtnManaOff = "xanSoundAlerts: Звуковые уведомления о мане теперь [|cFF99CC33ВЫКЛ|r]"
+L.ChkBtnManaInfo = "Включить звуковые уведомления о мане."
+
+L.ChkBtnOtherInfo = "Включить дополнительные звуковые уведомления [|cff40e0d0%s|r]."
+L.ChkBtnOtherOn = "xanSoundAlerts: Дополнительные звуковые уведомления [|cff40e0d0%s|r] теперь [|cFF99CC33ВКЛ|r]"
+L.ChkBtnOtherOff = "xanSoundAlerts: Дополнительные звуковые уведомления [|cff40e0d0%s|r] теперь [|cFF99CC33ВЫКЛ|r]"
